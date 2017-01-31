8a06b52c33c3a172919c44884100d25c23d93792
LucasRodriguez <LucasRodriguezBenitez@gmail.com>
2017-01-31 22:34:00 +0000

    Initial Commit
    Initial Commit

---
+++ Analyser.py
@@ -0,0 +1,124 @@
+#import regex
+import re
+import csv
+import nltk
+
+#remember to delete all tweets with a photo or link not just the photo or link
+
+#start process_tweet
+def processTweet(tweet):
+    # process the tweets
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub(r'((www.[^\s]+)|(https[^\s]+))','',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub(r'@[^\s]+','',tweet)
+    #Remove additional white    spaces
+    tweet = re.sub(r'[\s]+', ' ', tweet)
+    
+    tweet = re.sub(r'text:"','', tweet)
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    #trim
+    tweet = tweet.strip('\'"')
+    #get rid of the retweets in the text
+    tweet = tweet.strip('rt ')
+    return tweet
+#end
+
+#initialize stopWords
+stopWords = []
+
+#start replaceTwoOrMore
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+#end
+
+#start getStopWordList
+def getStopWordList(stopWordListFileName):
+    #read the stopwords file and build a list
+    stopWords = []
+    stopWords.append('AT_USER')
+    stopWords.append('URL')
+
+    fp = open(stopWordListFileName, 'r')
+    line = fp.readline()
+    while line:
+        word = line.strip()
+        stopWords.append(word)
+        line = fp.readline()
+    fp.close()
+    return stopWords
+#end
+
+#start getfeatureVector
+def getFeatureVector(tweet):
+    featureVector = []
+    #split tweet into words
+    words = tweet.split()
+    for w in words:
+        #replace two or more with two occurrences
+        w = replaceTwoOrMore(w)
+        #strip punctuation
+        w = w.strip('\'"?,.')
+        #check if the word stats with an alphabet
+        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
+        #ignore if it is a stop word
+        if(w in stopWords or val is None):
+            continue
+        else:
+            featureVector.append(w.lower())
+    return featureVector
+#end
+
+def extract_features(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in featureList:
+        features['contains(%s)' % word] = (word in tweet_words)
+    return features
+
+inpTweets = csv.reader(open('Training/Untitled.csv', 'rd'), delimiter=',', quotechar='|')
+stopWords = getStopWordList('stopwords.txt')
+featureList = []
+
+# Get tweet words
+tweets = []
+for row in inpTweets:
+    sentiment = row[0]
+    tweet = row[1]
+    processedTweet = processTweet(tweet)
+    featureVector = getFeatureVector(processedTweet)
+    featureList.extend(featureVector)
+    tweets.append((featureVector, sentiment));
+#end loop
+
+# Remove featureList duplicates
+featureList = list(set(featureList))
+
+# Extract feature vector for all tweets in one shote
+training_set = nltk.classify.util.apply_features(extract_features, tweets)
+
+NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
+
+
+with open ('text_tweets.txt','r') as tweets:
+    for tweet in tweets:
+        processedTestTweet = processTweet(tweet)
+        sentiment = NBClassifier.classify(extract_features(getFeatureVector(processedTestTweet.decode('unicode_escape').encode('ascii','ignore'))))
+        print processedTestTweet.decode('unicode_escape').encode('ascii','ignore')
+        print sentiment
+        with open('Summary/positive.txt','a') as pt:
+            if sentiment == "positive":
+                pt.write(processedTestTweet.decode('unicode_escape').encode('ascii','ignore') + "\n")
+        with open('Summary/negative.txt','a') as nt:
+            if sentiment=="negative":
+                nt.write(processedTestTweet.decode('unicode_escape').encode('ascii','ignore') + "\n")    
+                
+        
+                
+
+
---
+++ Tweety.py
@@ -0,0 +1,68 @@
+import tweepy 
+from tweepy import Stream 
+from tweepy import OAuthHandler 
+from tweepy.streaming import StreamListener 
+import time
+import argparse 
+import string 
+import json 
+import Keys as k 
+import csv
+import re 
+import operator  
+
+
+
+#This is a basic listener that just prints received tweets to stdout. 
+class StdOutListener(StreamListener):
+
+    def __init__(self, api=None):
+        super(StdOutListener, self).__init__()
+        #Initializes the tweet count. 
+        self.num_tweets = 0
+
+    def on_data(self, data):
+        print data
+        #Creates a file called fetched_tweets
+        with open('fetched_tweets.csv','a') as tf:
+            #increases the number of tweets by one.
+            self.num_tweets += 1
+            #while the number of tweets is less than 5 it continues to write them into the file created above
+            if self.num_tweets < 1000 :                
+                tf.write(data)     
+                return True
+            #When the number of tweets exceeds the one in the if statement it closes the connection and exits the progrm
+            else:                                   
+                with open("fetched_tweets.csv", 'r') as csvfile:
+                # get number of columns
+                    for line in csvfile.readlines():
+                        array = line.split(',')
+                        first_item = array[0]                
+                    num_columns = len(array)
+                    csvfile.seek(0)             
+                    reader = csv.reader(csvfile, delimiter=',')
+                    included_cols = [3] 
+                    for row in reader:
+                        content= (list(row[i] for i in included_cols) )            
+                        with open('text_tweets.txt','a') as tt:
+                            for s in content:
+                                tt.write(s + "\n") 
+                                print s
+                               
+                        
+                    return False
+
+    def on_error(self, status):
+        print status      
+
+        
+
+if __name__ == '__main__':
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(k.consumer_key, k.consumer_secret)
+    auth.set_access_token(k.access_token, k.access_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by keywords
+    stream.filter(languages=["en"], track=[("#TuesdayMotivation")])
